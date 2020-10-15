d62ba796f1331a2fbb27f82ea70177a74481a412
Ruturaj <24387949+RuturajS@users.noreply.github.com>
2020-10-15 13:48:45 +0530

    Update shri_Ram.py

--- shri_Ram.py
+++ shri_Ram.py
@@ -1,4 +1,4 @@
 #This code is written by Ruturaj Sharbidre 
 #Iamfondofreligionthat thought me how to live the life the base of all lives on earth is God shree Ram ,in the name of ram.
-for i in range(1000):
+for i in range(330000000):
     print("जय श्री राम जय राम जय जय राम")
